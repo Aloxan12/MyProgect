51f8e417ac82ecf70701b028d6935ad932e836a6
Aloxan12 <kvakvaaak@mail.ru>
2021-06-01 23:36:46 +0300

    upload new project and update components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./main/ui/header/Header";
+import {Redirect, Route, Switch} from "react-router-dom";
+import {Profile} from "./main/ui/components/profile/Profile";
+import {Test} from "./main/ui/components/test/Test";
+import {RegisterContainer} from "./main/ui/components/auth/register/RegisterContainer";
+import {LoginContainer} from "./main/ui/components/auth/login/LoginContainer";
+import {ForgotPasswordContainer} from './main/ui/components/auth/forgotPassword/ForgotPasswordContainer';
+import {SetPasswordContainer} from "./main/ui/components/auth/setPassword/SetPasswordContainer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header/>
+            <div className="App-content">
+                <Switch>
+                    <Route path='/login' render={() => <LoginContainer/>}/>
+                    <Route path='/registration' render={() => <RegisterContainer/>}/>
+                    <Route path='/profile' render={() => <Profile/>}/>
+                    <Route path='/forgot' render={() => <ForgotPasswordContainer/>}/>
+                    <Route path='/set-new-password/:token' render={() => <SetPasswordContainer/>}/>
+                    <Route path='/test' render={() => <Test/>}/>
+                    <Route path='/404' render={() => <div>404 PAGE NOT FOUND</div>}/>
+                    <Route path='*' render={() => <Redirect to={'/404'}/>}/>
+                </Switch>
+            </div>
+        </div>
+    );
 }
 
 export default App;
